778cd08ed4dc2f36bbf152233f5a69f32667e769
tarcisiolf <tarcisiolf0@gmail.com>
2025-02-19 19:14:38 -0300

    fix: i/o file names and get sentence item from test_data dict

--- 1_bilstmcrf_pytorch/train_test_70_30/evaluate_model.py
+++ 1_bilstmcrf_pytorch/train_test_70_30/evaluate_model.py
@@ -51,7 +51,7 @@ def evaluate_model(model, test_sentences_tensor, test_tags_tensor, test_data, wo
     test_predicted_tags = []
 
     for i in range(len(test_sentences_tensor)):
-        _, tags = dp.sentence_to_indices(test_data[i], word2index, tag2index)
+        _, tags = dp.sentence_to_indices(test_data[i]["sentence"], word2index, tag2index)
         predicted_tags = predict(model, test_sentences_tensor[i].unsqueeze(0), word2index)
 
         actual_tags = test_tags_tensor[i].tolist()
@@ -78,13 +78,13 @@ def convert(o):
         return o
 
 if __name__ == "__main__":
-    train_data = dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\train_data.json")
-    test_data = dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\test_data.json")
-    unique_tags = dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\unique_tags.json")
-    word2index = dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\word2index.json")
-    index2word = {int(k): v for k, v in dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\index2word.json").items()}
-    tag2index = dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\tag2index.json")
-    index2tag = {int(k): v for k, v in dp.load_json(r"1_bilstmcrf_pytorch\train_test_70_30\data\index2word.json").items()}
+    train_data = dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/train_data.json")
+    test_data = dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/test_data.json")
+    unique_tags = dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/unique_tags.json")
+    word2index = dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/word2index.json")
+    index2word = {int(k): v for k, v in dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/index2word.json").items()}
+    tag2index = dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/tag2index.json")
+    index2tag = {int(k): v for k, v in dp.load_json("1_bilstmcrf_pytorch/train_test_70_30/data/index2tag.json").items()}
 
     max_len = 512
     train_sentences_indices, train_tags_indices = dp.process_data(train_data, max_len, word2index, tag2index)
@@ -106,15 +106,16 @@ if __name__ == "__main__":
     for batch_size, embedding_dim, hidden_dim, lstm_dropout, learning_rate in itertools.product(
         param_grid["batch_size"], param_grid["embedding_dim"], param_grid["hidden_dim"], param_grid["lstm_dropout"], param_grid["learning_rate"]):
         
-        for i in range(5):
+        for i in range(10):
             logging.info(f"Evaluating model with batch_size={batch_size}, embedding_dim={embedding_dim}, hidden_dim={hidden_dim}_lstm_dropout={lstm_dropout}_learning_rate={learning_rate}")
-            print("\n")
+            
             config = Config(vocab_size, num_tags, embedding_dim, hidden_dim, lstm_dropout, learning_rate, epochs=10, batch_size=batch_size, padding_idx=word2index['<PAD>'])
 
             model = BiLSTM_CRF(config, word2index)
-            folder_name = r"1_bilstmcrf_pytorch\train_test_70_30\models"
-            model_file_name = folder_name+fr"\bilstm_crf_batch_size={batch_size}_embedding_dim={embedding_dim}_hidden_dim={hidden_dim}_lstm_dropout={lstm_dropout}_learning_rate={learning_rate}.pth"
-            model.load_state_dict(torch.load())
+            folder_name = "1_bilstmcrf_pytorch/train_test_70_30/models"
+            model_file_name = folder_name+f"/bilstm_crf_batch_size={batch_size}_embedding_dim={embedding_dim}_hidden_dim={hidden_dim}_lstm_dropout={lstm_dropout}_learning_rate={learning_rate}.pth"
+
+            model.load_state_dict(torch.load(model_file_name))
             model.eval()
 
             test_actual_tags, test_predicted_tags = evaluate_model(model, test_sentences_indices, test_tags_indices, test_data, word2index, tag2index, index2tag)
@@ -122,8 +123,8 @@ if __name__ == "__main__":
             report = classification_report(test_actual_tags, test_predicted_tags, output_dict=True)
             f1_score = report["macro avg"]["f1-score"]
 
-            logging.info(f"F1-score: {f1_score:.4f}")
-
-            output_file_name = "1_bilstmcrf_pytorch\train_test_70_30\metrics\"+"f"evaluation_report_batch_size={batch_size}_embedding_dim={embedding_dim}_hidden_dim={hidden_dim}_lstm_dropout={lstm_dropout}_learning_rate={learning_rate}_run_{i}.json"
+            logging.info(f"F1-score: {f1_score:.4f} | Run: {i+1}")
+        
+            output_file_name = f"1_bilstmcrf_pytorch/train_test_70_30/metrics/evaluation_report_batch_size={batch_size}_embedding_dim={embedding_dim}_hidden_dim={hidden_dim}_lstm_dropout={lstm_dropout}_learning_rate={learning_rate}_run_{i}.json"
             with open(output_file_name, "w") as f:
                 json.dump(report, f, indent=4, default=convert)
